7a1f7d9562bcf1798c9215a5f1cad4deff7e0fbd
ANTIBODY-PROCESSING <137297069+maheks75@users.noreply.github.com>
2024-06-19 10:55:57 +0100

    Update script4.py

--- script4.py
+++ script4.py
@@ -5,6 +5,7 @@ filename_4G = sys.argv[2]
 outfile_5G = sys.argv[3]
 outfile_4G = sys.argv[4]
 final_4g = sys.argv[4]
+print('script4 start')
 # Read the strings from the file
 with open(filename_5G, 'r') as file_5G:
     count_5G = 0
@@ -15,12 +16,12 @@ with open(filename_5G, 'r') as file_5G:
         parts_5G = string_5G.split(':')
         if int(parts_5G[1]) > 49:
             multiplestring_5G.append(parts_5G[0])
-            
-        
+
+
 with open(outfile_5G, 'w') as file_5G:
                 for i, stringgg_5G in enumerate(multiplestring_5G):
-                    print(stringgg_5G)
-                    file_5G.write(f"{stringgg_5G} \n")         
+                    #print(stringgg_5G)
+                    file_5G.write(f"{stringgg_5G} \n")
 with open(filename_4G, 'r') as file_4G:
     count_4G = 0
     twostringlist_4G = []
@@ -30,11 +31,10 @@ with open(filename_4G, 'r') as file_4G:
         parts_4G = string_4G.split(':')
         if int(parts_4G[1]) > 49:
             multiplestring_4G.append(parts_4G[0])
-            
-        
+
+
 with open(outfile_4G, 'w') as file_4G:
                 for i, stringgg_4G in enumerate(multiplestring_4G):
-                    print(stringgg_4G)
-                    file_4G.write(f"{stringgg_4G} \n")         
-     
-            
+                    #print(stringgg_4G)
+                    file_4G.write(f"{stringgg_4G} \n")
+print('script4 finish')
