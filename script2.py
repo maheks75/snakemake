975986bb1f4e487cc02465fe4e6d9fdc02cc4ccc
ANTIBODY-PROCESSING <137297069+maheks75@users.noreply.github.com>
2024-06-19 10:58:07 +0100

    Update script2.py

--- script2.py
+++ script2.py
@@ -8,7 +8,7 @@ output_file_5G = sys.argv[3]
 output_file_4G = sys.argv[4]
 
 sequences_written = 0
-
+print('script2 starts')
 with open(input_5g_txt_file, "r") as file, open(output_file_5G, 'w') as output_handle:
     strings = [line.strip() for line in file]
     for string in strings:
@@ -17,7 +17,7 @@ with open(input_5g_txt_file, "r") as file, open(output_file_5G, 'w') as output_h
             output_handle.write(extracted_sequence + "\n")
             sequences_written += 1
             #print("Extracted Sequence:", extracted_sequence)
-        
+
 with open(input_4g_txt_file, "r") as file, open(output_file_4G, 'w') as output_handle:
     strings = [line.strip() for line in file]
     for string in strings:
@@ -25,4 +25,5 @@ with open(input_4g_txt_file, "r") as file, open(output_file_4G, 'w') as output_h
             extracted_sequence = string[:12]
             output_handle.write(extracted_sequence + "\n")
             sequences_written += 1
+print('script2 finsih')
        
