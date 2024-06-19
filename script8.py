1997c6dffcd574872e4f46a7b82cc118cb6734ee
ANTIBODY-PROCESSING <137297069+maheks75@users.noreply.github.com>
2024-06-19 10:47:28 +0100

    Update script8.py

--- script8.py
+++ script8.py
@@ -7,21 +7,21 @@ sequence_list_file = sys.argv[4]
 
 sequence_full_list = []
 sequence_list_only = []
-
+print('script8 start')
 with open(output_file_full, 'w') as file:
     for i, record in enumerate(SeqIO.parse(input_fastq_file_5g, "fasta")):
         header_parts = record.description.split(":")
-        header = f">{i+1} {header_parts[1].strip()}"
+        header = f"> {header_parts[0].strip()}"
         sequence_data_5g = str(record.seq)
-        print(sequence_data_5g)
+        #print(sequence_data_5g)
         sequence_full_list.append(f"{header}\n{sequence_data_5g}")
         sequence_list_only.append(header_parts[1].strip())
 
     for i, record in enumerate(SeqIO.parse(input_fastq_file_4g, "fasta")):
         header_parts = record.description.split(":")
-        header = f">{i+1} {header_parts[1].strip()}"
+        header = f"> {header_parts[0].strip()}"
         sequence_data_4g = str(record.seq)
-        print(sequence_data_4g)
+        #print(sequence_data_4g)
         sequence_full_list.append(f"{header}\n{sequence_data_4g}")
         sequence_list_only.append(header_parts[1].strip())
 
@@ -32,6 +32,5 @@ with open(output_file_full, 'w') as file:
 with open(sequence_list_file, 'w') as seq_file:
     for seq_part in sequence_list_only:
         seq_file.write(seq_part + '\n')
-
-
+print('script8 finish')
 
